0d66de25103acc4f801bb75e1d077bb69f002ff1
Aditya Kumar <adityakumar5155@gmail.com>
2021-09-06 16:15:00 +0530

    <html> <body> Reduced RAM usage little bit. </body> </html>

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ while True:
         labelId = labelId + 1
         continue
     else:
-        feature = []
+        feature = [] #Freed some RAM by erasing unnecessary variables
         break
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
